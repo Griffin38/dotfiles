4c69a3c3ec8de1f97d11413b6f4e638c62d5a369
Pedro Lima <pedro@yarilabs.com>
2019-03-01 11:29:00 +0000

    upgrade bluettoth script

--- polybar-scripts/polybar-scripts/bluetooth.sh
+++ polybar-scripts/polybar-scripts/bluetooth.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
 if [ "$(systemctl is-active bluetooth.service)" = "active" ]; then
-	echo ""
+  if [ "$(echo -e "devices" | bluetoothctl | grep Mpow)" != '' ]; then
+    echo ""
+  else
+	  echo ""
+  fi
 else
 	echo ""
 fi
