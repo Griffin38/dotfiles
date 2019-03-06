345f031f2d970e02682b369afa7ebc425e5dda3f
Pedro Lima <pedro@yarilabs.com>
2019-03-06 23:53:25 +0000

    use pywal

--- polybar-scripts/polybar-scripts/bluetooth.sh
+++ polybar-scripts/polybar-scripts/bluetooth.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ "$(systemctl is-active bluetooth.service)" = "active" ]; then
-  if [ "$(echo -e "devices" | bluetoothctl | grep Mpow | wc -l)" > 1 ]; then
+  if [ "$(echo -e "devices" | bluetoothctl | grep Mpow | wc -l)" == 3 ]; then
     echo ""
   else
 	  echo ""
